972d96706bf07293bd1eea33fb4e1d5258bb43cc
garfex <ingenieriagarfex33@gmail.com>
2026-08-30 17:24:00 -0700

    feat(design-system): add GARFEX workstation foundations

---
+++ src/shared/design-system/GarfexLogoNegative.tsx
@@ -0,0 +1,11 @@
+export function GarfexLogoNegative() {
+  return (
+    <img
+      className="garfex-logo"
+      src="/docs/garfex-blanco-negativo.svg"
+      alt="GARFEX"
+      width="152"
+      height="48"
+    />
+  )
+}
